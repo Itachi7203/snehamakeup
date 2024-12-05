576053be94e0f5d1f605f1f00a430152dd13a6e7
aayush_sah <154677970+Itachi7203@users.noreply.github.com>
2024-12-05 16:43:33 +0530

    added all the files

---
+++ main.js
@@ -0,0 +1,4 @@
+// Redirect to Instagram on button click
+document.getElementById('buyNow').addEventListener('click', function() {
+    window.open('https://www.instagram.com/sneha_gupta__o?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==', '_blank');
+});
